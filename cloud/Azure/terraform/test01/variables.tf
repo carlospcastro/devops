ae80de8fd4a9663a2e0639f992179380819c88c6
Carlos Castro <carlospcastro@gmail.com>
2021-01-31 21:33:44 +0100

    AKS with terraform

---
+++ cloud/Azure/terraform/test01/variables.tf
@@ -0,0 +1,22 @@
+variable "service_principal_id" {
+}
+
+variable "service_principal_key" {
+}
+
+variable "tenant_id" {
+}
+
+variable "subscription_id" {
+}
+
+variable "ssh_key" {
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "kubernetes_version" {
+  default = "1.18.10"
+}
